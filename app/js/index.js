b6bbc4a4e1e7c59cfe0ff6395b6b0bc9249e4559
Victoria Trzcianowski <vtrzcianowski@gmail.com>
2018-01-04 17:19:26 -0500

    build

---
+++ app/js/index.js
@@ -0,0 +1,5 @@
+// require('./main.scss');
+
+let indexer = require('file-loader?name=../dist/index.html!../index.html');
+let slider = require('./slider.js');
+let custom = require('./modernizr.custom.79639.js');
